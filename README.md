95a7dbc2b05b1591da9d5cb7a15b89bc1e5b1866
nick-rebrik <nikolai.rubrik@it-lead.eu>
2023-04-08 17:40:05 +0200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# VPS-uploader
+# VPC Uploader
+
+### Description
+A simple project for uploading and downloading files to a VPS with download time tracking
+
+
+### Technologies
+
+- Python 3.10
+- Flask 2.2
+- Docker
+
+### Quick start
+
+1. Go to the directory 'infra' and run in command line:</br>
+```docker-compose up -d```
+
+### Authors
+
+- [Nick Rebrik](https://github.com/nick-rebrik)
---
+++ app.py
@@ -0,0 +1,44 @@
+import os.path
+from datetime import datetime
+
+from flask import Flask, g, render_template, request, send_from_directory
+from humanize import naturaldelta
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'files'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app = Flask(__name__)
+app.debug = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.before_request
+def record_upload_time():
+    g.start_time = datetime.now()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST' and 'file' in request.files:
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            upload_time = naturaldelta(datetime.now() - g.start_time)
+            return render_template(
+                'index.html',
+                filename=filename,
+                upload_time=upload_time
+            )
+    return render_template('index.html')
+
+
+@app.route('/download/<path:filename>/')
+def download(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run()
